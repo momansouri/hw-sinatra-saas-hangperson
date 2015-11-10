7c1d36decaf94e48fc1e22c0694f7512cee9c99a
M Mansouri <mohmanjdo@yahoo.fr>
2015-11-10 10:15:00 +0000

    implementation of the hangperson game

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -5,13 +5,49 @@ class HangpersonGame
 
   # Get a word from remote "random word" service
 
-  # def initialize()
+  #def initialize()
   # end
+  
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
+  def guess(letter)
+    raise ArgumentError if !letter || letter.empty? || !(letter =~ /[a-z]/i)
+    
+    letter.downcase!
+    if @guesses.include? letter or @wrong_guesses.include? letter
+      return false
+    end
+    if word.include? letter
+       @guesses += letter
+     else
+       @wrong_guesses += letter 
+    end
+    true
+  end
+  
+  def word_with_guesses
+    result = ''
+    @word.chars do |letter|
+      result += (@guesses.include? letter)? letter : '-'
+    end
+    result
+  end
+  
+  def check_win_or_lose
+    guessed_word = self.word_with_guesses
+    return :win if @word == guessed_word
+    return :lose if @wrong_guesses.length >= 7 && @word != guessed_word
+    return :play 
+  end
+  
   def self.get_random_word
     require 'uri'
     require 'net/http'
